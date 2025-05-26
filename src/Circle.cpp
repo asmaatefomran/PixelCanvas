0bf05be73860aaa40b2e87a77c0af09e92edbec8
Asmaa Atef <asmaatef6789@gmail.com>
2025-05-26 11:53:31 +0300

    Finished the filineg with lines

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 #include <cstdlib>
 
-Circle::Circle(HDC hdc) : hdc(hdc) {}
+Circle::Circle(HDC hdc) : hdc(hdc), line(hdc) {}
 
 void Circle::Draw8Points(int xc, int yc, int x, int y, COLORREF c) {
     SetPixel(hdc, xc + x, yc + y, c);
@@ -91,6 +91,7 @@ void Circle::DrawCircleModifiedMidpoint(int xc, int yc, int R, COLORREF c) {
 
 void Circle::FillWithCircles(int xc, int yc, int R) {
     int dec = R / 100;
+    if (dec == 0) dec = 1;
     while (R > 0) {
         R -= dec;
         COLORREF randomColor = RGB(rand() % 256, rand() % 256, rand() % 256);
@@ -101,20 +102,20 @@ void Circle::FillWithCircles(int xc, int yc, int R) {
 void Circle::Draw2Lines(int xc, int yc, int x, int y, COLORREF c, int quarter) {
     switch (quarter) {
     case 1:
-        DrawLineDDA(xc, yc, xc + x, yc, c);     
-        DrawLineDDA(xc, yc, xc, yc - y, c);     
+        line.DrawLineDDA(xc, yc, xc + x, yc, c);
+        line.DrawLineDDA(xc, yc, xc, yc - y, c);
         break;
     case 2:
-        DrawLineDDA(xc, yc, xc - x, yc, c);      
-        DrawLineDDA(xc, yc, xc, yc - y, c);      
+        line.DrawLineDDA(xc, yc, xc - x, yc, c);
+        line.DrawLineDDA(xc, yc, xc, yc - y, c);
         break;
     case 3:
-        DrawLineDDA(xc, yc, xc - x, yc, c);      
-        DrawLineDDA(xc, yc, xc, yc + y, c);      
+        line.DrawLineDDA(xc, yc, xc - x, yc, c);
+        line.DrawLineDDA(xc, yc, xc, yc + y, c);
         break;
     case 4:
-        DrawLineDDA(xc, yc, xc + x, yc, c);      
-        DrawLineDDA(xc, yc, xc, yc + y, c);      
+        line.DrawLineDDA(xc, yc, xc + x, yc, c);
+        line.DrawLineDDA(xc, yc, xc, yc + y, c);
         break;
     }
 }
@@ -124,7 +125,7 @@ void Circle::FillQuarter(int xc, int yc, int R, COLORREF c, int quarter) {
     int d = 1 - R;
     int d1 = 3, d2 = 5 - 2 * R;
     DrawCircleModifiedMidpoint(xc, yc, R, c);
-    Draw2Lines(hdc, xc, yc, x, y, c);
+    Draw2Lines(xc, yc, x, y, c, quarter);
     while (x < y) {
         if (d < 0) {
             d += d1;
@@ -138,7 +139,7 @@ void Circle::FillQuarter(int xc, int yc, int R, COLORREF c, int quarter) {
             x++;
             y--;
         }
-        Draw2Lines(hdc, xc, yc, x, y, c);
+        Draw2Lines(xc, yc, x, y, c, quarter);
     }
 }
 
