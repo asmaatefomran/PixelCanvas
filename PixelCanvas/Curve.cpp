f9449273c91386b6d3cc58245c55acdd9d7c87e0
Asmaa Atef <asmaatef6789@gmail.com>
2025-06-23 13:29:21 +0300

    Final version of the square and Rec

--- PixelCanvas/Curve.cpp
+++ PixelCanvas/Curve.cpp
@@ -17,7 +17,27 @@ struct Derivative {
 
 Curve::Curve(HDC hdc) : hdc(hdc) {}
 
-void Curve::DrawHermite(double x0, double y0, double x1, double y1, double t0x, double t0y, double t1x, double t1y, COLORREF color) {
+void Curve::DrawHermite(int x0, int y0, int x1, int y1, int t0, int t1, COLORREF color) {
+        for (double t = 0; t <= 1; t += 0.001) {
+            double h1 = 2 * pow(t, 3) - 3 * pow(t, 2) + 1;
+            double h2 = -2 * pow(t, 3) + 3 * pow(t, 2);
+            double h3 = pow(t, 3) - 2 * pow(t, 2) + t;
+            double h4 = pow(t, 3) - pow(t, 2);
+
+            int x = (int)(h1 * x0 + h2 * x1 + h3 * t0 + h4 * t1);
+            int y = (int)(h1 * y0 + h2 * y1);
+
+            SetPixel(hdc, x, y, color);
+        }
+}
+void Curve::FillWithHermite(int x1, int y1, int x2, int y2, COLORREF color) {
+    int step = 1;
+    for (int x = x1; x <= x2; x += step) {
+        DrawHermite(x, y1, x, y2, 40, 40, color);
+    }
+}
+
+void Curve::DrawHermite2(double x0, double y0, double x1, double y1, double t0x, double t0y, double t1x, double t1y, COLORREF color) {
     double h00, h10, h01, h11;
     for (double t = 0; t <= 1; t += 0.001) {
         h00 = 2 * t * t * t - 3 * t * t + 1;
@@ -30,7 +50,6 @@ void Curve::DrawHermite(double x0, double y0, double x1, double y1, double t0x, 
             SetPixel(hdc, round(x), round(y), color);
     }
 }
-
 void Curve::DrawCardinalSpline(POINT* pts, int n, double c, COLORREF color) {
     if (n < 4) return;
     struct Vector2 { double x, y; };
@@ -44,44 +63,26 @@ void Curve::DrawCardinalSpline(POINT* pts, int n, double c, COLORREF color) {
         Vector2 T1;
         T1.x = c1 * (P[i + 1].x - P[i - 1].x);
         T1.y = c1 * (P[i + 1].y - P[i - 1].y);
-        DrawHermite(P[i - 1].x, P[i - 1].y, P[i].x, P[i].y, T0.x, T0.y, T1.x, T1.y, color);
+        DrawHermite2(P[i - 1].x, P[i - 1].y, P[i].x, P[i].y, T0.x, T0.y, T1.x, T1.y, color);
         T0 = T1;
     }
 }
 
-void Curve::FillWithHermite(int x1, int y1, int x2, int y2, COLORREF color) {
-    int step = 1;
-    for (int x = x1; x <= x2; x += step) {
-        int yTop = y1;
-        int yBottom = y2;
-        if (yTop > yBottom) std::swap(yTop, yBottom);
-        for (int y = yTop; y <= yBottom; ++y) {
-            if (!clipWindowSet || (x >= clipMinX && x <= clipMaxX && y >= clipMinY && y <= clipMaxY))
-                SetPixel(hdc, x, y, color);
-        }
-    }
-}
 
 void Curve::DrawBezier(int x0, int y0, int x1, int y1, int x2, int y2, int x3, int y3, COLORREF color) {
-    for (double t = 0; t <= 1; t += 0.001) {
-        double mt = 1 - t;
-        int x = (int)(pow(mt, 3) * x0 + 3 * pow(mt, 2) * t * x1 +
-                                 3 * mt * pow(t, 2) * x2 + pow(t, 3) * x3);
-        int y = (int)(pow(mt, 3) * y0 + 3 * pow(mt, 2) * t * y1 +
-                                 3 * mt * pow(t, 2) * y2 + pow(t, 3) * y3);
-        SetPixel(hdc, x, y, color);
-    }
+        for (double t = 0; t <= 1; t += 0.001) {
+            double mt = 1 - t;
+            int x = (int)(pow(mt, 3) * x0 + 3 * pow(mt, 2) * t * x1 +
+                3 * mt * pow(t, 2) * x2 + pow(t, 3) * x3);
+            int y = (int)(pow(mt, 3) * y0 + 3 * pow(mt, 2) * t * y1 +
+                3 * mt * pow(t, 2) * y2 + pow(t, 3) * y3);
+            SetPixel(hdc, x, y, color);
+        }
 }
 
 void Curve::FillWithBezier(int x1, int y1, int x2, int y2, COLORREF color) {
     int step = 1;
     for (int y = y1; y <= y2; y += step) {
-        int xLeft = x1;
-        int xRight = x2;
-        if (xLeft > xRight) std::swap(xLeft, xRight);
-        for (int x = xLeft; x <= xRight; ++x) {
-            if (!clipWindowSet || (x >= clipMinX && x <= clipMaxX && y >= clipMinY && y <= clipMaxY))
-                SetPixel(hdc, x, y, color);
-        }
+        DrawBezier(x1, y, x1 + 20, y - 20, x2 - 20, y + 20, x2, y, color);
     }
-} 
+}
